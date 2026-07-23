0b30c6801319e92eef243e6befc45f25de25c3cf
bernacleboy11 <bernacleboy11@gmail.com>
2026-07-23 13:31:54 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,123 @@
+const CACHE_NAME = "warung-genz-v1";
+
+
+const FILES_TO_CACHE = [
+
+    "./",
+
+    "./index.html",
+
+    "./style.css",
+
+    "./script.js",
+
+    "./manifest.json"
+
+];
+
+
+self.addEventListener(
+
+    "install",
+
+    function(event) {
+
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+
+                .then(function(cache) {
+
+                    return cache.addAll(
+
+                        FILES_TO_CACHE
+
+                    );
+
+                })
+
+        );
+
+    }
+
+);
+
+
+self.addEventListener(
+
+    "activate",
+
+    function(event) {
+
+
+        event.waitUntil(
+
+            caches.keys()
+
+                .then(function(cacheNames) {
+
+
+                    return Promise.all(
+
+                        cacheNames.map(function(cacheName) {
+
+
+                            if (
+
+                                cacheName !== CACHE_NAME
+
+                            ) {
+
+
+                                return caches.delete(
+
+                                    cacheName
+
+                                );
+
+                            }
+
+                        })
+
+                    );
+
+                })
+
+        );
+
+    }
+
+);
+
+
+self.addEventListener(
+
+    "fetch",
+
+    function(event) {
+
+
+        event.respondWith(
+
+            caches.match(event.request)
+
+                .then(function(response) {
+
+
+                    if (response) {
+
+                        return response;
+
+                    }
+
+
+                    return fetch(event.request);
+
+                })
+
+        );
+
+    }
+
+);
